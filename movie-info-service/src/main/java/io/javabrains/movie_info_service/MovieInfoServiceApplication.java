3e938b6f4c8ab240512e853c03c092bd400cef61
Akshit Patel <akshitpatel.g@gmail.com>
2025-03-20 13:46:58 +0530

    [Akshit] Add . Code of movie-info-service

---
+++ movie-info-service/src/main/java/io/javabrains/movie_info_service/MovieInfoServiceApplication.java
@@ -0,0 +1,17 @@
+package io.javabrains.movie_info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+
+@SpringBootApplication
+
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+
+}
